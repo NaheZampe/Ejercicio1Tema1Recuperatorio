be57dd72aef7d74944013186aa4fdad4bf8dcb89
zampe <zampe@LAPTOP-8N3GQG6V>
2023-08-28 20:18:19 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1Tema1/Program.cs
@@ -0,0 +1,27 @@
+﻿using PrismaRectangularRecto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1Tema1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prisma");
+            Console.Write("Ingrese la altura del prisma:");
+            int altura = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el largo del prisma:");
+            int largo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el ancho del prisma:");
+            int ancho = Convert.ToInt32(Console.ReadLine());
+            Prisma p = new Prisma(altura,largo,ancho);
+
+            Console.WriteLine(p.MostrarDatos());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrismaRectangularRecto/Prisma.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismaRectangularRecto
+{
+    public class Prisma
+    {
+        private int _altura;
+        private int _largo;
+        private int _ancho;
+
+        public int GetAltura() => _altura;
+        public int GetAncho() => _ancho;
+        public int GetLargo() => _largo;
+
+        public Prisma(int altura, int largo, int ancho)
+        {
+            if (EsValido(altura,largo,ancho))
+            {
+                _altura = altura;
+                _largo = largo;
+                _ancho = ancho;
+            } 
+            
+        }
+
+        private bool EsValido(int altura, int largo, int ancho)
+        {
+            if (altura<=0||largo<=0||ancho<=0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public Prisma()
+        {
+            _altura = 1;
+            _largo = 1;
+            _ancho = 1;
+        }
+
+        private int AreaBase()
+        {
+            return (GetAncho() * GetLargo());
+        }
+        private int AreaAltura()
+        {
+            return (GetAltura() * GetAncho());
+        }
+        private int AreaAncho()
+        {
+            return (GetAltura() * GetLargo());
+        }
+
+        public int GetArea()
+        {
+            return (2 * (AreaAltura() + AreaAncho() + AreaBase()));
+        }
+        public int GetVolumen()
+        {
+            return (GetAltura()*GetAncho()*GetLargo());
+        }
+        public int GetDiagonal()
+        {
+            return (int)Math.Sqrt(Math.Pow(GetLargo(),2)+Math.Pow(GetAncho(),2)+Math.Pow(GetAltura(),2));
+        }
+        
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Prisma Rectangular Recto");
+            sb.AppendLine($"Area.......:{GetArea()}");
+            sb.AppendLine($"Volumen....:{GetVolumen()}");
+            sb.AppendLine($"Diagonal...:{GetDiagonal()}");
+
+            return sb.ToString();
+        }
+
+    }
+    
+}
